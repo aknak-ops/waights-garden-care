ecd529ef4dd64cb59cf30f07ef7c3dbf7357ed09
aknak-ops <alexwaights@gmail.com>
2025-09-30 14:15:26 +0100

    chore: init Waights Garden Care site

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'wgc-cache-v2';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './assets/css/style.css',
+  './assets/js/main.js',
+  './manifest.json',
+  './assets/images/icon-192.png',
+  './assets/images/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(URLS_TO_CACHE)));
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=>{
+      return resp || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+    ))
+  );
+});
