0903a5c25f916538b995e0f4c101c7012883da79
aknak-ops <alexwaights@gmail.com>
2025-11-09 10:45:04 +0000

    Cleanup: consolidate assets, fix paths, purify favicons

--- assets/favicons/perf.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var hero = document.querySelector('.hero img');
-  if (hero){
-    hero.setAttribute('loading','eager');
-    hero.setAttribute('fetchpriority','high');
-    hero.setAttribute('decoding','async');
-  }
-})();
